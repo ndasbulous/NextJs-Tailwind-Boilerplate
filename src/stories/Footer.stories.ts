102d5a3a6d30ba19f90b3525fddfd9f8855e3b68
Emanuel Anggit <ndasbulous@gmail.com>
2025-08-29 00:06:41 +0700

    Add sample stories

---
+++ src/stories/Footer.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { fn } from 'storybook/test';
+import Footer from '../components/Footer';
+
+import '../../app/globals.css'
+
+const meta = {
+  title: 'Example/Footer',
+  component: Footer,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+  args: {
+    onLogin: fn(),
+    onLogout: fn(),
+    onCreateAccount: fn(),
+  },
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LoggedIn: Story = {
+  args: {
+    user: {
+      name: 'Jane Doe',
+    },
+  },
+};
+
+export const LoggedOut: Story = {};
